cc0d6aefe0d1a20259b8932b20e47d28866cc98f
kszulik <62623028+kszulik@users.noreply.github.com>
2024-11-06 19:29:33 +0100

    dodanie plików bootloadera

---
+++ bl_ster_co/STM32CubeIDE/Debug/Application/User/generated/image_small_btn_pressed.d
@@ -0,0 +1,4 @@
+Application/User/generated/image_small_btn_pressed.o: \
+ C:/Users/kszul/Desktop/SterownikCo/SterownikCo/TouchGFX/generated/images/src/image_small_btn_pressed.cpp \
+ ../../Middlewares/ST/touchgfx/framework/include/touchgfx/hal/Config.hpp
+../../Middlewares/ST/touchgfx/framework/include/touchgfx/hal/Config.hpp:
