cdb87de39fda256e8c8a650f6776e416092ceb1f
kszulik <62623028+kszulik@users.noreply.github.com>
2024-11-07 11:16:02 +0100

    usuwanie niepotrzebnych plików

--- ap_ster_co/TouchGFX/build/STM32F746-DISCO/TouchGFX/generated/images/src/image_small_btn_pressed.d
+++
@@ -1,5 +0,0 @@
-TouchGFX/build/STM32F746-DISCO/TouchGFX/generated/images/src/image_small_btn_pressed.o: \
- TouchGFX/generated/images/src/image_small_btn_pressed.cpp \
- Middlewares/ST/touchgfx/framework/include/touchgfx/hal/Config.hpp
-
-Middlewares/ST/touchgfx/framework/include/touchgfx/hal/Config.hpp:
--- bl_ster_co/TouchGFX/build/STM32F746-DISCO/TouchGFX/generated/images/src/image_btn_long_pressed.d
+++
@@ -1,5 +0,0 @@
-TouchGFX/build/STM32F746-DISCO/TouchGFX/generated/images/src/image_btn_long_pressed.o: \
- TouchGFX/generated/images/src/image_btn_long_pressed.cpp \
- Middlewares/ST/touchgfx/framework/include/touchgfx/hal/Config.hpp
-
-Middlewares/ST/touchgfx/framework/include/touchgfx/hal/Config.hpp:
